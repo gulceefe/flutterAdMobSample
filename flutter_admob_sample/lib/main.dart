9c8cee85750b7648a9460aa8e88f5daa288dcfdc
tivorlu <gulceninmesenesi@hotmail.com>
2022-02-01 21:33:39 +0300

    Reklam Eklemeden Önce

--- flutter_admob_sample/lib/main.dart
+++ flutter_admob_sample/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AdMob Sample',
       theme: ThemeData(
         // This is the theme of your application.
         //
